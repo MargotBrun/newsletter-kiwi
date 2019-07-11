7e60239c011d1ac79cef31ca1860d8e8a894a15e
Paulin Trognon <paulin.trognon@gmail.com>
2019-07-12 10:33:18 +1200

    revert react-ga

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactGA from 'react-ga';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import SubscriptionPage from './pages/Subscription/Subscription';
@@ -9,24 +8,17 @@ import AusNzQuizPage from './pages/AusNzQuiz/AusNzQuizPage';
 import ListNewsletters from './pages/ListNewsletters/ListNewsletters';
 import PicturesNewsletters from './pages/PicturesNewsletters/PicturesNewsletters';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    ReactGA.initialize('UA-143186341-1');
-  }
-
-  render = () => (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={SubscriptionPage} />
-          <Route path="/desinscription" exact component={UnsubscriptionPage} />
-          <Route path="/politique-confidentialite" exact component={ConfidentialityPage} />
-          <Route path="/quiz" exact component={AusNzQuizPage} />
-          <Route path="/liste" exact component={ListNewsletters} />
-          <Route path="/pictures" exact component={PicturesNewsletters} />
-        </Switch>
-      </Layout>
-    </Router>
-  );
-}
+export default () => (
+  <Router>
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={SubscriptionPage} />
+        <Route path="/desinscription" exact component={UnsubscriptionPage} />
+        <Route path="/politique-confidentialite" exact component={ConfidentialityPage} />
+        <Route path="/quiz" exact component={AusNzQuizPage} />
+        <Route path="/liste" exact component={ListNewsletters} />
+        <Route path="/pictures" exact component={PicturesNewsletters} />
+      </Switch>
+    </Layout>
+  </Router>
+);
