9fafe2de6b5227e235e4fd58f1d41b2bbaf083bc
Paulin Trognon <paulin.trognon@gmail.com>
2019-07-12 10:17:51 +1200

    test react ga

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,23 @@ import AusNzQuizPage from './pages/AusNzQuiz/AusNzQuizPage';
 import ListNewsletters from './pages/ListNewsletters/ListNewsletters';
 import PicturesNewsletters from './pages/PicturesNewsletters/PicturesNewsletters';
 
-ReactGA.initialize('UA-143186341-1');
+export default class App extends React.Component {
+  componentDidMount = () => {
+    ReactGA.initialize('UA-143186341-1');
+  };
 
-export default () => (
-  <Router>
-    <Layout>
-      <Switch>
-        <Route path="/" exact component={SubscriptionPage} />
-        <Route path="/desinscription" exact component={UnsubscriptionPage} />
-        <Route path="/politique-confidentialite" exact component={ConfidentialityPage} />
-        <Route path="/quiz" exact component={AusNzQuizPage} />
-        <Route path="/liste" exact component={ListNewsletters} />
-        <Route path="/pictures" exact component={PicturesNewsletters} />
-      </Switch>
-    </Layout>
-  </Router>
-);
+  render = () => (
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={SubscriptionPage} />
+          <Route path="/desinscription" exact component={UnsubscriptionPage} />
+          <Route path="/politique-confidentialite" exact component={ConfidentialityPage} />
+          <Route path="/quiz" exact component={AusNzQuizPage} />
+          <Route path="/liste" exact component={ListNewsletters} />
+          <Route path="/pictures" exact component={PicturesNewsletters} />
+        </Switch>
+      </Layout>
+    </Router>
+  );
+}
