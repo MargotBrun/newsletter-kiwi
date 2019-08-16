07c861eec9c0accbea4ed981843a8fda9ad78673
Paulin Trognon <paulin.trognon@gmail.com>
2019-08-16 14:37:40 +1200

    finish bird integration

--- src/pages/BirdsPage/Bellbird/Bellbird.jsx
+++ src/pages/BirdsPage/Bellbird/Bellbird.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import ShinyLink from '../../../components/ShinyLink/ShinyLink';
-import BellbirdImg from './bellbird.jpg';
-import BellbirdSound from './bellbird.mp3';
+import bellbirdImg from './bellbird.jpg';
+import bellbirdSound from './bellbird.mp3';
+import playImg from '../play.png';
 
-export default () => (
+export default ({ handleOpenPopup }) => (
   <div className="card">
-    <img src={BellbirdImg} className="card-img-top" alt="Auckland, quartier de Ponsonby" />
+    <button type="button" className="BirdsPage_button" onClick={handleOpenPopup('wfifZO49bSM')}>
+      <img src={bellbirdImg} className="card-img-top" alt="Korimako (Bellbird)" />
+      <div className="BirdsPage_button_hover">
+        <img src={playImg} />
+      </div>
+    </button>
     <div className="card-body">
       <h5 className="card-title">Korimako (Bellbird)</h5>
-      <small>Version Audio</small>
       <audio controls className="BirdsPage_audio">
-        <source src={BellbirdSound} type="audio/mpeg" />
+        <source src={bellbirdSound} type="audio/mpeg" />
       </audio>
     </div>
-    <div className="card-footer">
-      <ShinyLink
-        href="http://sh1.sendinblue.com/nc4glolx4c.html?t=1560470731"
-        target="_blank"
-        rel="noopener noreferrer"
-        multicolor
-      >
-        <small>Voir la version vidéo</small>
+    <div className="card-footer BirdsPage_footer">
+      <ShinyLink multicolor onClick={handleOpenPopup('wfifZO49bSM')}>
+        <svg width="25px" height="25px" viewBox="0 0 512 512">
+          <path d="M480,0H32C14.313,0,0,14.328,0,32v448c0,17.688,14.313,32,32,32h448c17.688,0,32-14.312,32-32V32   C512,14.328,497.688,0,480,0z M320,32l32,64h-64l-32-64H320z M192,32l32,64h-64l-32-64H192z M32,32h32l32,64H32V32z M480,480H32   V128h448V480z M480,96h-64l-32-64h64l32,32V96z M355.375,289.688c15.75,7.875,15.75,20.75,0,28.625l-166.75,83.375   C172.875,409.562,160,401.594,160,384V224c0-17.594,12.875-25.563,28.625-17.688L355.375,289.688z" />
+        </svg>{' '}
+        Voir la version vidéo
       </ShinyLink>
     </div>
   </div>
--- src/pages/BirdsPage/BirdsPage.jsx
+++ src/pages/BirdsPage/BirdsPage.jsx
@@ -3,20 +3,55 @@ import './BirdsPage.css';
 import Page from '../../components/Page/Page';
 import Bellbird from './Bellbird/Bellbird';
 
-export default () => (
-  <Page title="Kei Te Waiata Ngā Manu" className="BirdsPage">
-    <h2 className="BirdsPage_subtitle">Le chant des oiseaux de Nouvelle-Zélande</h2>
-    <div className="ListNewsletters_content_cards">
-      <div className="card-deck">
-        <Bellbird />
-        <Bellbird />
-        <Bellbird />
-      </div>
-      <div className="card-deck">
-        <Bellbird />
-        <Bellbird />
-        <Bellbird />
-      </div>
-    </div>
-  </Page>
-);
+export default class BirdsPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isPopupOpen: false,
+      videoId: null,
+    };
+  }
+
+  handleOpenPopup = videoId => () => {
+    this.setState({
+      videoId,
+      isPopupOpen: true,
+    });
+  };
+
+  render() {
+    const { isPopupOpen, videoId } = this.state;
+    return (
+      <Page title="Kei Te Waiata Ngā Manu" className="BirdsPage">
+        {isPopupOpen && (
+          <div
+            className="BirdsPage_popup"
+            role="button"
+            tabIndex={0}
+            onClick={() => {
+              this.setState({ isPopupOpen: false });
+            }}
+          >
+            <iframe
+              className="BirdsPage_popup_iframe"
+              title="bellbird"
+              src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+        )}
+
+        <h2 className="BirdsPage_subtitle">Le chant des oiseaux de Nouvelle-Zélande</h2>
+        <div className="ListNewsletters_content_cards">
+          <div className="card-deck">
+            <Bellbird handleOpenPopup={this.handleOpenPopup} />
+            <Bellbird handleOpenPopup={this.handleOpenPopup} />
+            <Bellbird handleOpenPopup={this.handleOpenPopup} />
+          </div>
+        </div>
+      </Page>
+    );
+  }
+}
